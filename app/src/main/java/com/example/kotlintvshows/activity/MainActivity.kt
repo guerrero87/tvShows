e038100755d5bb3c13e9ee42440db414c822538b
mrgod777 <guerrerosanti@gmail.com>
2020-10-28 12:41:28 -0300

    created models for TopTvSHows request

--- app/src/main/java/com/example/kotlintvshows/activity/MainActivity.kt
+++ app/src/main/java/com/example/kotlintvshows/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.kotlintvshows
+package com.example.kotlintvshows.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.kotlintvshows.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/kotlintvshows/tmdbAPI/model/Genre.kt
@@ -0,0 +1,10 @@
+package com.example.kotlintvshows.tmdbAPI.model
+
+class Genre(val genre: List<Int>)
+
+/*
+"genre_ids": [
+    16,
+    10759
+    ]
+ */
